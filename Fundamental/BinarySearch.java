7575181a59a9e643bd9a426d9234e83e9b1881a1
Satish Nikale <satishnikale7@gmail.com>
2024-10-02 00:22:40 +0530

    Add binary search problem

--- Fundamental/BinarySearch.java
+++ Fundamental/BinarySearch.java
@@ -1,34 +1,34 @@
 public class BinarySearch{
 
-    public static int binarySearch(int arr[], int target){
-        // Initialise starting and ending index for traversing.
-        int low = 0, high = arr.length-1;
-        // low <= high if remain single element.
-        while(low <= high){
-            int mid = (low+high)/2;
-        // return index + 1 if found.
-            if(arr[mid] == target){
-                return mid + 1;
+        public static int binarySearch(int arr[], int target){
+            // Initialise starting and ending index for traversing.
+            int low = 0, high = arr.length-1;
+            // low <= high if remain single element.
+            while(low <= high){
+                int mid = (low+high)/2;
+            // return index + 1 if found.
+                if(arr[mid] == target){
+                    return mid + 1;
+                }
+            // if arr[mid] < target then go for second half (low = mid +1) 
+            // else search in first half (high = mid - 1)
+                if(arr[mid] < target){
+                    low = mid + 1;
+                } else {
+                    high = mid-1;
+                }
             }
-        // if arr[mid] < target then go for second half (low = mid +1) 
-        // else search in first half (high = mid - 1)
-            if(arr[mid] < target){
-                low = mid + 1;
-            } else {
-                high = mid-1;
-            }
+            // if not found return -1
+            return -1;
         }
-        // if not found return -1
-        return -1;
-    }
-    public static void main(String[] args) {
-
-        int arr[] = {2, 3, 5, 6, 7, 8};
-        int target = 8;
-
-        System.out.println(binarySearch(arr, target));
-
-        // Time complexity : O(N)
-        // Space complexity : O(1)
-    }
+        public static void main(String[] args) {
+    
+            int arr[] = {2, 3, 5, 6, 7, 8};
+            int target = 8;
+    
+            System.out.println(binarySearch(arr, target));
+    
+            // Time complexity : O(N)
+            // Space complexity : O(1)
+        }
 }
---
+++ Fundamental/PrefixSum.java
@@ -0,0 +1,5 @@
+public class PrefixSum {
+    public static void main(String[] args) {
+        int arr[] = {4, 5, 8, 7, 3, 2, 1, 9};
+    }
+}
